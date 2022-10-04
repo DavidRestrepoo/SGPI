2e705182a842bb08117e8759cc0fa54c6a87a32d
ESTUDIANTE <ESTUDIANTE@DESKTOP-0R9U54R>
2022-10-04 10:52:44 -0500

    Agregar archivos de proyecto.

---
+++ SGPI/Controllers/CoordinadorController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SGPI.Controllers
+{
+    public class CoordinadorController : Controller
+    {
+        public IActionResult BuscarCoordinador()
+        {
+            return View();
+        }
+
+        public IActionResult ProgramarCoordinador()
+        {
+            return View();
+        }
+
+        public IActionResult Homologacion()
+        {
+            return View();
+        }
+
+        public IActionResult EntrevistaCoordinador()
+        {
+            return View();
+        }
+
+        public IActionResult ReportesCoordinador()
+        {
+            return View();
+        }
+
+    }
+}

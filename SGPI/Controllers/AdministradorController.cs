791ec7f15f7be9d4dd009ca3546c233d7397b853
DavidRestrepoo <davidres14@gmail.com>
2022-10-20 11:40:29 -0500

    se agregó cod en login y controlador

--- SGPI/Controllers/AdministradorController.cs
+++ SGPI/Controllers/AdministradorController.cs
@@ -5,29 +5,64 @@ namespace SGPI.Controllers
 {
     public class AdministradorController : Controller
     {
-        SGPI_BDContext BDContext=new SGPI_BDContext();
+        SGPI_BDContext BDContext = new SGPI_BDContext();
         public IActionResult Login()
         {
             ///Create
-            TblUsuario usr = new TblUsuario();
-            usr.PrimerNombre = "Mauricio";
-            usr.SegundoNombre=String.Empty;
-            usr.PrimerApellido = "Amariles";
-            usr.SegundoApellido = "Camacho";
-            usr.Email = "yonosequeponer@gmail.com";
-            usr.Iddoc = 1;
-            usr.Idgenero = 1;
-            usr.NumeroDocumento = "123456789";
-            usr.VcPassword = "123";
+            //TblUsuario usr = new TblUsuario();
+            //usr.PrimerNombre = "Mauricio";
+            //usr.SegundoNombre=String.Empty;
+            //usr.PrimerApellido = "Amariles";
+            //usr.SegundoApellido = "Camacho";
+            //usr.Email = "yonosequeponer@gmail.com";
+            //usr.Iddoc = 1;
+            //usr.Idgenero = 1;
+            //usr.NumeroDocumento = "123456789";
+            //usr.VcPassword = "123";
 
-            BDContext.Add(usr);
-            BDContext.SaveChanges();
+            //BDContext.Add(usr);
+            //BDContext.SaveChanges();
 
             //Query
 
             //Update
 
+            var usr = BDContext.TblUsuarios.Where(cursor => cursor.Idusuario == 1).FirstOrDefault();
+            if (usr != null)
+            {
+                usr.SegundoNombre = "Tronchatoro";
+                BDContext.TblUsuarios.Update(usr);
+                BDContext.SaveChanges();
+            }
             //Delete
+            var usuarioEliminar = BDContext.TblUsuarios.Where(cursor => cursor.Idusuario == 1).FirstOrDefault();
+            BDContext.TblUsuarios.Remove(usuarioEliminar);
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(TblUsuario user)
+        {
+            string numerodoc = user.NumeroDocumento;
+            string password = user.VcPassword;
+
+            var usuarioLogin=BDContext.TblUsuarios.Where(consulta => consulta.NumeroDocumento == numerodoc && consulta.VcPassword=password).FirstOrDefault();
+            if (usuarioLogin != null)
+            {
+                switch (usuarioLogin.Idrol)
+                {
+                    case 1:
+                    case 2:
+                    case 3:
+                    default:
+                }
+            }
+            else
+            {
+                return ViewBag.mensaje = "Usuario no existe o usuario/contraseña no no valida";
+                
+            }
             return View();
         }
 

fd87106a1ea0e204e4ad40fd4dbe91ac6c201c52
david <david@DERO>
2022-11-15 16:48:36 -0500

    estudiante, usuario t pago funcional

--- SGPI/Controllers/EstudianteController.cs
+++ SGPI/Controllers/EstudianteController.cs
@@ -25,39 +25,11 @@ namespace SGPI.Controllers
             {
                 return Redirect("/Estudiante/ActualizarEstudiante/?Idusuario=");
             }
-        }
-
-        //public IActionResult ActualizarEstudiante()
-        //{
-        //    return View();
-
-        //}
+        }      
 
         [HttpPost]
         public IActionResult ActualizarEstudiante(TblUsuario usuarios)
-        {            
-        //    int numerodocumento = usuarios.Idusuario;
-        //    var usuarioActualizar = BDContext.TblUsuarios.Where(consulta => consulta.Idusuario == numerodocumento).FirstOrDefault();
-        //    usuarioActualizar.NumeroDocumento = usuarios.NumeroDocumento;
-        //    usuarioActualizar.Iddoc = usuarios.Iddoc;
-        //    usuarioActualizar.PrimerNombre = usuarios.PrimerNombre;
-        //    usuarioActualizar.SegundoNombre = usuarios.SegundoNombre;
-        //    usuarioActualizar.PrimerApellido = usuarios.PrimerApellido;
-        //    usuarioActualizar.SegundoApellido = usuarios.SegundoApellido;
-        //    usuarioActualizar.Idgenero = usuarios.Idgenero;
-        //    usuarioActualizar.Idprograma = usuarios.Idprograma;
-        //    usuarioActualizar.Email = usuarios.Email;
-        //    //TblEstudiante estudiante = new TblEstudiante();
-        //    //estudiante.Idusuario = usuarios.Idusuario;
-        //    //estudiante.Egresado = false;
-        //    //estudiante.Archivo = "";
-        //    //BDContext.Add(estudiante);
-        //    //BDContext.SaveChanges();
-        //    ViewBag.mensaje = "Se ha actualizado con exito";
-        //    BDContext.Update(usuarioActualizar);
-        //    BDContext.SaveChanges();
-        //    return Redirect("/Estudiante/ActualizarEstudiante/?Idusuario=" + usuarioActualizar.Idusuario);
-
+        {     
             usuarios.Idrol=3;
            ViewBag.Mensaje ="Se Actualizo con exito";
             ViewBag.idusuario=usuarios.Idusuario;
@@ -66,53 +38,37 @@ namespace SGPI.Controllers
             ViewBag.TblPrograma = BDContext.TblProgramas.ToList();
             ViewBag.TblGenero = BDContext.TblGeneros.ToList();
             ViewBag.TblTipoDocumento = BDContext.TblTipoDocumentos.ToList();
-            return View(usuarios);
+            return Redirect("/Estudiante/ActualizarEstudiante/?Idusuario="+usuarios.Idusuario);
     }
 
-        public IActionResult PagoEstudiante()
+        public IActionResult PagoEstudiante(int? Idusuario)
         {
-            //TblUsuario usuario = new TblUsuario();
-            //var usr = BDContext.TblUsuarios.Where(consulta => consulta.Idusuario == IdUsuario).FirstOrDefault();
-            //ViewBag.idusuario = usr.Idusuario;
-            //List<TblPago> listpago = new List<TblPago>();
-            //listpago = BDContext.TblPagos.ToList();
-            //ViewBag.listapgs = listpago;
+            TblUsuario usuario =new TblUsuario();
+            var usr = BDContext.TblUsuarios.Where(consulta => consulta.Idusuario== Idusuario).FirstOrDefault();
+            ViewBag.Idusuario = usr.Idusuario;
             
             return View();
         }
 
         [HttpPost]
-        public IActionResult PagoEstudiante(TblPago usuario)
+        public IActionResult PagoEstudiante(int? Idusuario, TblPago usuario)
         {
-            usuario.Estado = true;
-            
+            TblUsuario usr = BDContext.TblUsuarios.Find(Idusuario);
+
+            usuario.Estado = true;            
             ViewBag.mensaje = "Pago Enviado";
             BDContext.TblPagos.Add(usuario);
             BDContext.SaveChanges();
 
-            //var user = BDContext.TblUsuarios.Where(consulta => consulta.Idusuario == idusuario).FirstOrDefault();
+            TblEstudiante estudiante = new TblEstudiante();
+            estudiante.Archivo = "";
+            estudiante.Idusuario= usr.Idusuario;
+            estudiante.Idpago = usuario.IdPagos;
+            estudiante.Egresado = false;
 
-            //TblUsuario usr = BDContext.TblUsuarios.Find(IdUsuario);
-
-            //BDContext.TblPagos.Add(pago);
-            //BDContext.SaveChanges();
-
-
-            //TblEstudiante est = new TblEstudiante();
-            //est.Idusuario = usr.Idusuario;
-            //est.Idpago = pago.IdPagos;
-            //est.Egresado = false;
-
-            //BDContext.TblEstudiantes.Add(est);
-            //BDContext.SaveChanges();
-
-            //TblUsuario usuario = new TblUsuario();
-            //var usrc = BDContext.TblUsuarios.Where(consulta => consulta.Idusuario == IdUsuario).FirstOrDefault();
-            //ViewBag.idusuario = usrc.Idusuario;
-
-            //List<TblPago> listpago = new List<TblPago>();
-            //listpago = BDContext.TblPagos.ToList();
-            //ViewBag.listapgs = listpago;
+            BDContext.TblEstudiantes.Add(estudiante);
+            BDContext.SaveChanges();
+           
             return View();
         }
     }
